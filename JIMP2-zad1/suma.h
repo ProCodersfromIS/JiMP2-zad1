4afc98f9e11f611869eea31b2352b72462726490
BeataG <bgielbaga@gmail.com>
2014-05-08 18:31:47 +0200

    próbny commit

---
+++ JIMP2-zad1/suma.h
@@ -0,0 +1,20 @@
+#include "suma_spec.h"
+
+template <class T> //definicja szablonu
+double suma(T input, int len)
+{
+	double result = 0;
+
+	cout << endl;
+	for (int i = 0; i < len; ++i)
+	{
+		result += input[i];
+		cout << input[i];
+		if (i < len - 1)
+			cout << " + ";
+		else 
+			cout << " = " << result << endl;
+	}
+		
+	return result;
+}
