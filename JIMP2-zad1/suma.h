d4b7cbb7d5718191dfb79bec82ec157476cf9df2
BeataG <bgielbaga@gmail.com>
2014-05-11 19:21:10 +0200

    Zmiany w sumach
    Czy mógłbyś rzucić okiem czy dobrze to zrobiłam??

--- JIMP2-zad1/suma.h
+++ JIMP2-zad1/suma.h
@@ -1,6 +1,16 @@
+/*
+* \file suma.h
+* \author Beata Gie³baga
+* \brief Suma.h zawiera definicje oraz cia³o szablonu funkcji
+*/
+
 #include "suma_spec.h"
 
+//deklaracja szablonu
 template <class T>
+
+//funkcja suma przyjmuje argumenty: parametryczny input, dzieki ktoremu odnosimy siê do sk³adników sumy
+//oraz len typu int, który okresla dlugosc tablicy(ilosc skladnikow) 
 double suma(T input, int len)
 {
 	double result = 0;
@@ -10,11 +20,11 @@ double suma(T input, int len)
 	{
 		result += input[i];
 		cout << input[i];
-		if (i < len - 1)
+		if (i < len - 1) //instrukcja warunkowa umo¿liwiajaca wyswietlenie sumy w postaci a + b +... = suma
 			cout << " + ";
 		else 
 			cout << " = " << result << endl;
 	}
 		
-	return result;
+	return result; //funkcja zwraca zmienna result która jest sum¹ wszystkich sk³adników input
 }
--- JIMP2-zad1/suma_spec.h
+++ JIMP2-zad1/suma_spec.h
@@ -1,16 +1,25 @@
+/*
+* \file suma.h
+* \author Beata Gie³baga
+* \brief Suma.h zawiera definicje oraz cia³o szablonu funkcji
+*/
+
+//pisanie deklaracji szablonu(<template<>) mo¿na pomin¹æ dla funkcji specjalizowanej 
 using namespace std;
 
+//funkcja suma przyjmuje argumenty: wsakaŸnik do tablicy charów, dziêki któremu odwo³amy siê do poszczególnych elementów tablicy
+//oraz len typu int który okresla dlugosc tablicy
 double suma(char** input, int len)
 {
 	cout << endl;
 	double result = 0;
-	string helper = "";
+	string helper = ""; //zmienna pomocnicza
 
 	for (int i = 0; i < len; ++i)
 	{
 		helper += input[i];
 		cout << input[i];
-		if (i < len - 1)
+		if (i < len - 1) //instrukcja warunkowa aby by³a mo¿liwosc wypisania sumy znakow w taki sposob a+b+c=abc
 			cout << " + ";
 		else
 		{
@@ -19,5 +28,5 @@ double suma(char** input, int len)
 		}
 	}
 
-	return result;
+	return result; //funkcja zwraca zmienna result której wartosc równa siê dlugosc wyrazu powstalego przez skeljenie wartosci input
 }
